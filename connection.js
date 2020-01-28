4c3a4938a637d2e88a72a4076f29d26cd7240511
Luis <luis.vazquez.r25@gmail.com>
2020-01-27 22:00:13 -0300

    se desinstalo express

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 /**
  * conectamos a la base de datos
  */
-mongoose.connect('mongodb://localhost:27017/ecommercerDB', {useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/ecommerceDB', {useNewUrlParser:true});
 
 module.exports = mongoose;
 
