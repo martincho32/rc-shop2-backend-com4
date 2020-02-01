b81df44317f66093a1c3a2d4c958f3357c53e7b3
Martín Alejandro Barros <martinbarros86@gmail.com>
2020-01-31 21:10:02 -0300

    Variable port created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Product = require('./products');
 const app = express();
+const port = 8080;
 
 app.use(express.json());
 
@@ -22,6 +23,6 @@ app.post('/api/products', async function (req, res) {
 });
 
 
-app.listen(8080, function() {
-    console.log("Server listening on port 8080...");
+app.listen(port, function() {
+    console.log(`Server listening on port ${port}...`);
   });
