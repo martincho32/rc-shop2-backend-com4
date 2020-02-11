232363dfb5b199cb80405736c75e10a95f77b447
giomanconi <giomanconi@hotmail.com>
2020-02-11 19:35:08 -0300

    insertJson.js file es6 problems fixed | Fixed img path from json

--- insertJson.js
+++ insertJson.js
@@ -1,14 +1,13 @@
-import { products } from './itemsForDB.json';
-import { remove, insertMany } from './products';
-
+const json = require('./itemsForDB.json');
+const Product = require('./products');
 /*
  - To Insert JSON file into DB type 'npm run insert'
  - You need to have the server running for this to work
 */
 async function insertProducts() {
     try {
-        await remove({});
-        await insertMany(products);
+        await Product.remove({});
+        await Product.insertMany(json.products);
         console.log('JSON file added succesfully. Closing process...');
         process.exit();
     }
