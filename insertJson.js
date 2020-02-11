4211324664862ea456d05f60d74ac89bba2b3e14
giomanconi <giomanconi@hotmail.com>
2020-02-11 18:44:11 -0300

    code cleanup

--- insertJson.js
+++ insertJson.js
@@ -1,17 +1,19 @@
 import { products } from './itemsForDB.json';
 import { remove, insertMany } from './products';
 
-
+/*
+ - To Insert JSON file into DB type 'npm run insert'
+ - You need to have the server running for this to work
+*/
 async function insertProducts() {
     try {
         await remove({});
-        console.log('Se borro completamente');
         await insertMany(products);
+        console.log('JSON file added succesfully. Closing process...');
         process.exit();
     }
     catch (e) {
         console.log(e);
     }
 };
-
 insertProducts();
