e31966774aa59314f4e841760c04afe4779e72e3
giomanconi <giomanconi@hotmail.com>
2020-02-20 00:39:42 -0300

    eslint warnings solved

--- routes/email.js
+++ routes/email.js
@@ -5,10 +5,10 @@ const sgMail = require('@sendgrid/mail'); //sendgrid library to send emails
 const Subscriptors = require("../model/subscriptors");
 
 router.use(cors()); //so the browser doesn't restrict data, without it Sendgrid will not send
-sgMail.setApiKey('');//api key
+sgMail.setApiKey('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');//api key
 
 router.get('/api/subscriptors', async function (req, res) {
-  try{
+  try {
     const subscriptors = await Subscriptors.find();
     res.send(subscriptors);
   }
@@ -27,23 +27,30 @@ router.post('/api/subscriptors', async function (req, res) {
   }
 });
 
+router.post('/api/send-email', async function (req, res) {
+  try {
+    const subscriptors = await Subscriptors.find(req.body);
+
+    if (subscriptors) {
+      res.send('Ya estas subscripto a nuestras ultimas noticias')
+    }
+    else {
+      const email = await Subscriptors.create(req.body);
+      res.send("Email : " + email+ " was added to Subscriptors collection");
+      
+      const msg = { //Sendgrid Data Requirements
+        to: req.body,
+        from: "giomanconi@hotmail.com", //should come from backend
+        subject: "Rolling Shop", //should come from backend
+        text: "Gracias por suscribirte" //should come from backend
+      }
+
+      sgMail.send(msg)//Send Email
+        .then(console.log("Email was sent"));
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 module.exports = router;
-//SG.emiwWnUnTpa7MmpRaIdyJg.lNvU0mSTa113-RPxGVDJwBpEWCjhFzsHwQv6MkrWiGIO78
-
-// app.get('/send-email', (req,res) => {
-    
-//   //Get Variables from query string in the search bar
-//   const { recipient } = req.query; 
-
-//   //Sendgrid Data Requirements
-//   const msg = {
-//       to: recipient, 
-//       from: "giomanconi@hotmail.com", //should come from backend
-//       subject: "Rolling Shop", //should come from backend
-//       text: "Gracias por suscribirte" //should come from backend
-//   }
-
-//   //Send Email
-//   sgMail.send(msg)
-//   .then((msg) => console.log(recipient));
-// });
