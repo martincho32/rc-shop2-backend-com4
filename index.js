c39acaad31b602fac0af7373b5dd3941c2d5fcf7
Martín Alejandro Barros <martinbarros86@gmail.com>
2020-03-04 00:36:57 -0300

    userLogin&Register first version

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const Product = require('./model/products');
 const email = require('./routes/email');
 const app = express();
 const port = 8080;
+const authRoute = require('./routes/auth');
+const posRoute = require('./routes/posts');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 
 app.use(express.json());
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/email', email);
+app.use('/api/user', authRoute);
+app.use('/api/posts', posRoute);
+
+
+
+
 
 
 //Search by brand (this can be changed to search by other property, we could add a "keywords" property to the schema)
